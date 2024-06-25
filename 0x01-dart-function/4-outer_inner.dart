e9be4dfc5c762503213b2294fb809a1cbcbc0b80
unknown <5628@holbertonstudents.com>
2024-06-25 21:15:39 +0200

    adjust ex4 for it call to every name

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -4,10 +4,7 @@ void outer(String name, String id) {
 
 String inner(String name) {
   List<String> nameParts = name.split(' ');
-  if (nameParts.isNotEmpty) {
-    String firstName = nameParts[0];
-    return 'B.$firstName';
-  } else {
-    return '';
-  }
+  return nameParts.length >= 2
+    ? '${nameParts[1][0]}.${nameParts[0]}'
+    : name;
 }
