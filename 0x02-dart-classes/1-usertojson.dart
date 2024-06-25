f1fa6c5f3242b0e65e1b1e8467368de0417b9906
unknown <5628@holbertonstudents.com>
2024-06-25 23:14:40 +0200

    ex 1

---
+++ 0x02-dart-classes/1-usertojson.dart
@@ -0,0 +1,11 @@
+class User {
+  var name;
+  int? age;
+  double? height;
+
+  User({this.name, this.age, this.height});
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'age': age, 'height': height};
+  }
+}
